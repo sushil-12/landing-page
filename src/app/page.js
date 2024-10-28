3a68189cebdc5016875b56f403f34be3f43df8f1
sushil <sushil.kumar@infostride.com>
2024-10-28 18:42:33 +0530

    pushed

--- src/app/page.js
+++ src/app/page.js
@@ -34,11 +34,8 @@ function Home() {
     <>
       <main id="pagepiling">
         <div className="sections bg-secondary">
-          <div
-            id="section1"
-            className="section pp-scrollable text-gray-700 body-font min-h-screen flex items-center bg-primary"
-          >
-            <div className="mx-auto flex px-4 md:px-16 lg:px-36 md:flex-row flex-col items-center justify-center w-full ">
+          <div id="section1" className="section pp-scrollable text-gray-700 body-font min-h-screen flex items-start lg:items-center bg-primary py-20 lg:py-0">
+            <div className="mx-auto flex px-4 md:px-16 lg:px-36 md:flex-row flex-col items-start lg:items-center justify-center w-full ">
               <div className="lg:flex-grow max-w-6xl text-center md:text-left lg:text-left">
                 <h1 className="text-[38px] md:text-responsive-64 lg:text-responsive-64  mb-4 font-semibold max-w-xl poppins-semibold text-white text-center md:text-left lg:text-left">
                   From the Roots of
@@ -47,7 +44,7 @@ function Home() {
                   <br />
                   Rise Together.
                 </h1>
-                <img className="object-cover object-center rounded block md:hidden lg:hidden" alt="hero" src="/assets/balkan-org.svg" />
+                <img className="object-cover object-center rounded block md:hidden lg:hidden w-96 items-center" alt="hero" src="/assets/balkan-org.svg" />
                 <p className="mb-8 leading-relaxed text-white max-w-xl poppins-regular text-[18px] text-center md:text-left lg:text-left">
                   Unite with the Balkans' strongest network. Empowering progress, innovation, and shared tradition. Be part of a community that drives growth and fosters collaboration across the region.
                 </p>
@@ -61,21 +58,21 @@ function Home() {
 
           <div
             id="section2"
-            className="section pp-scrollable text-gray-700 px-4 md:px-16 lg:px-36 body-font bg-secondary min-h-screen flex items-center "
+            className="section pp-scrollable text-gray-700 px-4 md:px-16 lg:px-36 body-font bg-secondary min-h-screen flex items-start lg:items-center py-20 lg:py-0 "
           >
             <StandardCard />
           </div>
 
           <div
             id="section3"
-            className="section pp-scrollable text-gray-700 px-4 md:px-16 lg:px-36 bg-primary min-h-screen  md:flex lg:flex items-center"
+            className="section pp-scrollable text-gray-700 px-4 md:px-16 lg:px-36 bg-primary min-h-screen  md:flex lg:flex items-start lg:items-center py-20 lg:py-0"
           >
             <InfoSection />
           </div>
 
           <div
             id="section4"
-            className="section text-gray-700 px-4 md:px-16 lg:px-36 body-font bg-secondary min-h-screen flex flex-col items-center mb-4"
+            className="section text-gray-700 px-4 md:px-16 lg:px-36 body-font bg-secondary min-h-screen flex flex-col items-start lg:items-center mb-4 py-20 lg:py-0"
           >
             <ContactForm />
           </div>
