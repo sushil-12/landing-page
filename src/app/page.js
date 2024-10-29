2e1aa729b50ad435cfc17b7a5a10f99b8a2a6bbc
sushil <sushil.kumar@infostride.com>
2024-10-29 10:45:09 +0530

    added script tag

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,8 @@ import StandardCard from "./components/StandardCard";
 // Import pagePiling CSS only
 import "pagepiling.js/dist/jquery.pagepiling.css";
 import homeJSonLd from "./schema/homeJsonLd";
+import Script from "next/script";
+import Head from "next/head";
 
 function Home() {
   useEffect(() => {
@@ -22,7 +24,7 @@ function Home() {
         $("#pagepiling").pagepiling({
           direction: "vertical",
           scrollingSpeed: 700,
-          navigation: false
+          navigation: false,
         });
       };
 
@@ -32,13 +34,18 @@ function Home() {
 
   return (
     <>
+      <Head>
+        <Script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(homeJSonLd) }}
+        />
+      </Head>
       <main id="pagepiling">
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(homeJSonLd) }}
-      />
         <div className="sections bg-secondary">
-          <div id="section1" className="section pp-scrollable text-gray-700 body-font min-h-screen flex items-start md:items-center lg:items-center bg-primary py-20 lg:py-0">
+          <div
+            id="section1"
+            className="section pp-scrollable text-gray-700 body-font min-h-screen flex items-start md:items-center lg:items-center bg-primary py-20 lg:py-0"
+          >
             <div className="mx-auto flex px-4 md:px-16 lg:px-36 md:flex-row flex-col items-start lg:items-center justify-center w-full ">
               <div className="lg:flex-grow max-w-6xl text-center md:text-left lg:text-left">
                 <h1 className="text-[38px] md:text-responsive-64 lg:text-responsive-64  mb-4 font-semibold max-w-xl poppins-semibold text-white text-center md:text-left lg:text-left">
@@ -48,14 +55,25 @@ function Home() {
                   <br />
                   Rise Together.
                 </h1>
-                <img className="object-cover object-center rounded block md:hidden lg:hidden w-96 items-center justify-self-center" alt="hero" src="/assets/balkan-org.svg" />
+                <img
+                  className="object-cover object-center rounded block md:hidden lg:hidden w-96 items-center justify-self-center"
+                  alt="hero"
+                  src="/assets/balkan-org.svg"
+                />
                 <p className="mb-8 leading-relaxed text-white max-w-xl poppins-regular text-[18px] text-center md:text-left lg:text-left">
-                  Unite with the Balkans' strongest network. Empowering progress, innovation, and shared tradition. Be part of a community that drives growth and fosters collaboration across the region.
+                  Unite with the Balkans' strongest network. Empowering
+                  progress, innovation, and shared tradition. Be part of a
+                  community that drives growth and fosters collaboration across
+                  the region.
                 </p>
                 <button className="custom-button">Request to Join</button>
               </div>
               <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 hidden md:block lg:block">
-                <img className="object-cover object-center rounded" alt="hero" src="/assets/balkan-org.svg" />
+                <img
+                  className="object-cover object-center rounded"
+                  alt="hero"
+                  src="/assets/balkan-org.svg"
+                />
               </div>
             </div>
           </div>
